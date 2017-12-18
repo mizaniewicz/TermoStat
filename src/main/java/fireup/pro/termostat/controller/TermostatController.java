015acfbcf5a3d834d881e8f0c7e35c2f32678b0d
Mirosław Zaniewicz <mizaniewicz@gmail.com>
2017-12-18 13:00:49 +0100

    commit

--- src/main/java/fireup/pro/termostat/controller/TermostatController.java
+++ src/main/java/fireup/pro/termostat/controller/TermostatController.java
@@ -19,19 +19,14 @@ public class TermostatController {
         this.termostatService = termostatService;
     }
 
-    @GetMapping("/temperature")
-    public Termostat showTemperature(@RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime date) {
-        return termostatService.getTemperatureByDate(date);
-    }
-
     @GetMapping("/temperature/{id}")
     public Termostat showTemperature(@PathVariable("id") Long id) {
         return termostatService.getTemperatureById(id);
     }
 
     @GetMapping("/temperatures/{startdate}/{enddate}")
-    public List<Termostat> temperaturesBetweenDates(@PathVariable("startdate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime startDate,
-                                                    @PathVariable("enddate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime endDate) {
+    public List<Termostat> temperaturesBetweenDates(@PathVariable("startdate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
+                                                    @PathVariable("enddate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDate) {
         return termostatService.getTEmeraturesBetweenDates(startDate, endDate);
     }
 
